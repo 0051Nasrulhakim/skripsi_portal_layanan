eb2e6e75aa2cf5edeb1b70992fca501a9e4e79ca
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-16 23:10:10 +0700

    dashboard 20%

---
+++ app/Views/page/dashboard.php
@@ -0,0 +1,59 @@
+<?= $this->include('template/publik_header') ?>
+    <div class="banner">
+        <div class="judul">
+            <div class="j_1">
+                PORTAL
+            </div>
+            <div class="j_2">
+                 LAYANAN ONLINE
+            </div>
+        </div>
+        <div class="informasi">
+            <button class="btn">Informasi</button>
+        </div>
+        <div class="row_card">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text">
+                        Tersedia
+                    </div>
+                    <div class="angka">
+                        7 Layanan
+                    </div>  
+                    <div class="tombol">
+                        <button class="btn">Lihat</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="text">
+                        Layanan Pengaduan
+                    </div>
+                    <div class="angka">
+                        24 Jam
+                    </div>
+                    <div class="tombol">
+                        <button class="btn">Form Pengaduan</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="text">
+                        Jam Operasional
+                    </div>
+                    <div class="angka">
+                        07.00 - 15.00
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="section_dashboard">
+            <h1>Hello, world!</h1>
+        </div>
+    </div>
+
+<?= $this->include('template/publik_footer') ?>
---
+++ app/Views/template/publik_header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DINPERINAKER KOTA PEKALONGAN</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script> -->
+        
+        <!-- data tables -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        
+        <!-- get css from publik -->
+        <link rel="stylesheet" href="<?= base_url('assets/style.css') ?>">
+        <!-- sweeet alert -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="s_navbar_1">
+                <img src="<?= base_url()?>img/header.png" alt="">
+            </div>
+            <div class="s_navbar_2">
+                <!-- tombol login -->
+                <div class="login">
+                    <a href="<?= base_url('login')?>" class="btn">Login</a>
+                </div>
+                <!-- buat menu dropdown -->
+                <!-- <div class="layanan">
+                    <div class="dashboard">
+                        dashboard
+                    </div>
+
+                </div> -->
+            </div>
+        </div>
+        
