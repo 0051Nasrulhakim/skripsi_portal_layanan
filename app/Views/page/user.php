d94fe0d9a7644d21b509bf90c5d09ebdc2254eee
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-19 22:59:03 +0700

    Update user.php

--- app/Views/page/user.php
+++ app/Views/page/user.php
@@ -13,42 +13,43 @@
                     <div class="mb-3 row">
                         <label for="nama" class="col-sm-3 col-form-label">Nama Lengkap</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="nama" name="nama">
+                            <input type="text" class="form-control" id="nama" name="nama" disabled>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="tanggal_lahir" class="col-sm-3 col-form-label">Tanggal Lahir</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="tanggal_lahir" name="tanggal_lahir">
+                            <input type="text" class="form-control" id="tanggal_lahir" name="tanggal_lahir" disabled>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="alamat" name="alamat">
+                            <input type="text" class="form-control" id="alamat" name="alamat" disabled>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="jenis_kelamin" class="col-sm-3 col-form-label">Jenis Kelamin</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+                            <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin" disabled>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="username" class="col-sm-3 col-form-label">Username</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="username" name="username">
+                            <input type="text" class="form-control" id="username" name="username" disabled>
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="password" class="col-sm-3 col-form-label">Password</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="password" name="password">
+                            <input type="text" class="form-control" id="password" name="password" disabled>
                         </div>
                     </div>
                     <div class="btn_user">
-                        <button type="button" class="btn btn-primary">Simpan</button>
-                        <button type="button" class="btn btn-danger">Batal</button>
+                        <button type="button" class="btn btn-danger" id="btn_batal" hidden>Batal</button>
+                        <button type="button" class="btn btn-primary" id="btn_update" onclick="update()">Update</button>
+                        <button type="button" class="btn btn-primary" id="btn_simpan" onclick="simpan()" hidden>Simpan</button>
                     </div>
                 </div>
                 <div class="page_histori_pengajuan" id="page_histori_pengajuan" hidden>
@@ -58,16 +59,22 @@
                                 <tr>
                                     <th>No</th>
                                     <th>Jenis Pengajuan</th>
+                                    <th>Tanggal</th>
+                                    <th>keterangan</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td>1</td>
                                     <td>asasas</td>
+                                    <td>12-05-2023</td>
+                                    <td>Acc</td>
                                 </tr>
                                 <tr>
                                     <td>1</td>
                                     <td>asasas</td>
+                                    <td>12-05-2023</td>
+                                    <td>Acc</td>
                                 </tr>
                             </tbody>
                         </table>
@@ -80,16 +87,19 @@
                                     <tr>
                                         <th>No</th>
                                         <th>pengaduan</th>
+                                        <th>tanggal</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
                                         <td>1</td>
                                         <td>asasas</td>
+                                        <td>12-05-2023</td>
                                     </tr>
                                     <tr>
                                         <td>1</td>
                                         <td>asasas</td>
+                                        <td>12-05-2023</td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -151,6 +161,19 @@
         $('#btn_profile').addClass('active');
         $('#btn_pengaduan').removeClass('active');
         $('#btn_histori_pengajuan').removeClass('active');
+    }
+    function update(){
+        $('#btn_update').attr('hidden', true);
+        $('#btn_simpan').attr('hidden', false);
+        $('#btn_batal').attr('hidden', false);
+
+        $('#nama').removeAttr('disabled');
+        $('#alamat').removeAttr('disabled');
+        $('#no_hp').removeAttr('disabled');
+        $('#jenis_kelamin').removeAttr('disabled');
+        $('#tanggal_lahir').removeAttr('disabled');
+        $('#username').removeAttr('disabled');
+        $('#password').removeAttr('disabled');
     }
 </script>
 <?= $this->include('template/publik_footer') ?>
