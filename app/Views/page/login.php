9a48e4a1db73274dbea12561de44f87947a9775c
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-22 00:00:55 +0700

    Update login.php

--- app/Views/page/login.php
+++ app/Views/page/login.php
@@ -8,6 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="<?= base_url('assets/login.css') ?>">
 </head>
 <body>
@@ -35,7 +38,7 @@
                             </div>
                         </div>
                         <div class="button">
-                            <button class="btn btn-primary">Login</button>
+                            <button type="submit" class="btn btn-primary">Login</button>
                         </div>
                         <div class="keterangan">
                             *Bagi yang belum mempunyai akun silahkan klik <div class="disini" onclick="disini_register()">register</div>
@@ -43,7 +46,7 @@
                     </form>
                 </div>
                 <div class="form_register" hidden>
-                    <form method="post" id="f_register">
+                    <form id="f_register">
                         <div class="mb-3 row">
                             <label for="nama" class="col-sm-3 col-form-label">Nama</label>
                             <div class="col-sm-9">
@@ -53,19 +56,19 @@
                         <div class="mb-3 row">
                             <label for="email" class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="email" id="email_register">
+                                <input type="text" class="form-control" name="email_register" id="email_register">
                             </div>
                         </div>
                         <div class="mb-3 row">
                             <label for="username" class="col-sm-3 col-form-label">username</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="username" id="username_register">
+                                <input type="text" class="form-control" name="username_register" id="username_register">
                             </div>
                         </div>
                         <div class="mb-3 row">
                             <label for="password" class="col-sm-3 col-form-label">password</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="password" id="password_register">
+                                <input type="text" class="form-control" name="password_register" id="password_register">
                             </div>
                         </div>
                         <div class="button">
@@ -78,7 +81,7 @@
                                 </div>
                             </div>
                             <div class="tombol">
-                                <button class="btn btn-primary">Daftar</button>
+                                <button type="submit" class="btn btn-primary">Daftar</button>
                             </div>
                         </div>
                         
@@ -105,6 +108,35 @@
             document.getElementById('username').value = "";
             document.getElementById('password').value = "";
         }
+        $('#f_login').submit(function(e) {
+            e.preventDefault();
+            alert($('#f_login').serialize());
+
+            console.log($('#f_login').serialize());
+            // $('#f_login').serialize(),
+            // $.ajax({
+            //     url: '',
+            //     type: '',
+            //     data: $('#update_form').serialize(),
+            //     //data: new FormData(this),
+            //     success: function(response) {
+            //         // your code here
+            //     }
+            // });
+        })
+        $('#f_register').submit(function(e) {
+            e.preventDefault();
+            alert($('#f_register').serialize());
+            // $.ajax({
+            //     url: '',
+            //     type: '',
+            //     data: $('#update_form').serialize(),
+            //     //data: new FormData(this),
+            //     success: function(response) {
+            //         // your code here
+            //     }
+            // });
+        })
     </script>
 </body>
 </html>
