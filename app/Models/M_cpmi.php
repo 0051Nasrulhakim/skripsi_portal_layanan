18d0c7d83b8419a9aa7b15d766f0649955da8172
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-22 22:23:48 +0700

    Create M_cpmi.php

---
+++ app/Models/M_cpmi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_cpmi extends Model
+{
+    protected $table      = 'cpmi';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['nik','nama','alamat','nama_perusahaan','jabatan','negara_tujuan','tanggal_berangkat','pas_foto','foto_ktp','foto_kk','foto_akta_Kelahiran','foto_surat_nikah','foto_ijazah_terakhir','foto_surat_perjanjian','foto_medical_check_up','foto_ak1','tanggal_pengajuan'];
+}
