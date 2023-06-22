ea52b66292595536ec605848e18d8e18522cf0f2
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-22 22:23:36 +0700

    Create M_bkk.php

---
+++ app/Models/M_bkk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_bkk extends Model
+{
+    protected $table      = 'bkk';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['nama_bkk','penanggung_jawab','alamat_bkk','struktur_bkk','akta_pendirian_bkk','rencana_kerja_bkk','dokumen_pendirian_bkk','pass_foto_kepsek','tanggal_pengajuan'];
+}
