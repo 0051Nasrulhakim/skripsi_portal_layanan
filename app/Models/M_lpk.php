f1bd952374588086719de427e463729b89d70bfb
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-22 22:23:56 +0700

    Create M_lpk.php

---
+++ app/Models/M_lpk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class M_lpk extends Model
+{
+    protected $table      = 'lpk';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['alamat_lembaga','bidang_pelatihan','foto_bukti_kepemilikan','foto_keputusan','foto_keterangan_domisili','foto_npwp_perusahaan','identitas_kepala_lpk','nama_lembaga','nomor_telfon','npwp_perusahaan','penanggung_jawab','profile_lpk','tanggal_pengajuan'];
+}
