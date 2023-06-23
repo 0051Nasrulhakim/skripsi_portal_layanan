d9eeef9d41279422cc79770c453baf3d6697af3c
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-23 22:52:06 +0700

    Update M_lpk.php

--- app/Models/M_lpk.php
+++ app/Models/M_lpk.php
@@ -9,5 +9,5 @@ class M_lpk extends Model
     protected $table      = 'lpk';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['alamat_lembaga','bidang_pelatihan','foto_bukti_kepemilikan','foto_keputusan','foto_keterangan_domisili','foto_npwp_perusahaan','identitas_kepala_lpk','nama_lembaga','nomor_telfon','npwp_perusahaan','penanggung_jawab','profile_lpk','tanggal_pengajuan'];
+    protected $allowedFields = ['id_user','alamat_lembaga','bidang_pelatihan','foto_bukti_kepemilikan','foto_keputusan','foto_keterangan_domisili','foto_npwp_perusahaan','identitas_kepala_lpk','nama_lembaga','nomor_telfon','npwp_perusahaan','penanggung_jawab','profile_lpk','tanggal_pengajuan'];
 }
