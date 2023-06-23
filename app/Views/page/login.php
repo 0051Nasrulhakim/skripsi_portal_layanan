1c300f9c158aa3a3ae58176aa1872d1fe072e1e5
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-23 22:52:42 +0700

    Update login.php

--- app/Views/page/login.php
+++ app/Views/page/login.php
@@ -11,6 +11,9 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="<?= base_url('assets/login.css') ?>">
 </head>
 <body>
@@ -24,17 +27,19 @@
                     Login Portal Layanan Dinperinaker
                 </div>
                 <div class="form_login">
-                    <form method="post" id="f_login">
+                    <form id="f_login">
                         <div class="mb-3 row">
                             <label for="username" class="col-sm-3 col-form-label">username</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" id="username" name="username">
+                                <input type="text" class="form-control" id="username" name="username" onkeyup="r_username_login_e()">
+                                <div class="form-text text-danger" id="username_login_error"></div>
                             </div>
                         </div>
                         <div class="mb-3 row">
                             <label for="inputPassword" class="col-sm-3 col-form-label">Password</label>
                             <div class="col-sm-9">
-                                <input type="password" class="form-control" id="password" name="password">
+                                <input type="text" class="form-control" id="password" name="password" onkeyup="r_password_login_e()">
+                                <div class="form-text text-danger" id="password_login_error"></div>
                             </div>
                         </div>
                         <div class="button">
@@ -50,25 +55,30 @@
                         <div class="mb-3 row">
                             <label for="nama" class="col-sm-3 col-form-label">Nama</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" id="nama_register" name="nama">
-                            </div>
+                                <input type="text" class="form-control" id="nama_register" name="nama_register" onkeyup="r_nama_register_e()">
+                                <div class="form-text text-danger" id="nama_register_error"></div>
+                            </div>  
                         </div>
                         <div class="mb-3 row">
                             <label for="email" class="col-sm-3 col-form-label">Email</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="email_register" id="email_register">
+                                <input type="text" class="form-control" name="email_register" id="email_register" onkeyup="r_email_register_e()">
+                                <!-- pesan error dengan label -->
+                                <div class="form-text text-danger" id="email_register_error"></div>
                             </div>
                         </div>
                         <div class="mb-3 row">
                             <label for="username" class="col-sm-3 col-form-label">username</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="username_register" id="username_register">
+                                <input type="text" class="form-control" name="username_register" id="username_register" onkeyup="r_username_register_e()">
+                                <div class="form-text text-danger" id="username_register_error"></div>
                             </div>
                         </div>
                         <div class="mb-3 row">
                             <label for="password" class="col-sm-3 col-form-label">password</label>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" name="password_register" id="password_register">
+                                <input type="text" class="form-control" name="password_register" id="password_register" onkeyup="r_password_register_e()">
+                                <div class="form-text text-danger" id="password_register_error"></div>
                             </div>
                         </div>
                         <div class="button">
@@ -84,7 +94,6 @@
                                 <button type="submit" class="btn btn-primary">Daftar</button>
                             </div>
                         </div>
-                        
                     </form>
                 </div>
             </div>
@@ -108,35 +117,91 @@
             document.getElementById('username').value = "";
             document.getElementById('password').value = "";
         }
+
         $('#f_login').submit(function(e) {
             e.preventDefault();
-            alert($('#f_login').serialize());
-
-            console.log($('#f_login').serialize());
-            // $('#f_login').serialize(),
-            // $.ajax({
-            //     url: '',
-            //     type: '',
-            //     data: $('#update_form').serialize(),
-            //     //data: new FormData(this),
-            //     success: function(response) {
-            //         // your code here
-            //     }
-            // });
+            $.ajax({
+                url: 'http://localhost:8080/user/login',
+                type: 'POST',
+                data: $('#f_login').serialize(),
+                //data: new FormData(this),
+                success: function(res_login) {
+                    console.log(res_login);
+                    if(res_login.status == 'error'){
+                        // masukkan pesan error ke dalam div
+                        $('#username_login_error').text(res_login.username);
+                        $('#password_login_error').text(res_login.password);
+                    }else{
+                        // swall
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Berhasil',
+                            text: 'Login Berhasil',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                        // redirect
+                        setTimeout(function() {
+                            window.location.href = 'http://localhost:8080/home';
+                        }, 1000);
+                    }
+                }
+            });
         })
+
         $('#f_register').submit(function(e) {
             e.preventDefault();
-            alert($('#f_register').serialize());
-            // $.ajax({
-            //     url: '',
-            //     type: '',
-            //     data: $('#f_register').serialize(),
-            //     //data: new FormData(this),
-            //     success: function(response) {
-            //         // your code here
-            //     }
-            // });
+            $.ajax({
+                url: 'http://localhost:8080/user/register',
+                type: 'post',
+                data: $('#f_register').serialize(),
+                // data: new FormData(this),
+                success: function(response) {
+                    console.log(response);
+                    if(response.status == 'error'){
+                        // masukkan pesan error ke dalam div
+                        $('#nama_register_error').text(response.errors.nama_lengkap);
+                        $('#email_register_error').text(response.errors.email);
+                        $('#username_register_error').text(response.errors.username);
+                        $('#password_register_error').text(response.errors.password);
+                    }
+                    if(response.status == 'success'){
+                        // swall
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Berhasil',
+                            text: 'Pendaftaran Berhasil Silahkan Login',
+                            showConfirmButton: true,
+                            
+                        })
+                        .then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = "<?= base_url()?>home/login";
+                            }
+                        })
+                    }
+                }
+            });
         })
+        function r_nama_register_e(){
+            $('#nama_register_error').text('');
+        }
+        function r_email_register_e(){
+            $('#email_register_error').text('');
+        }
+        function r_username_register_e(){
+            $('#username_register_error').text('');
+        }
+        function r_password_register_e(){
+            $('#password_register_error').text('');
+        }
+        function r_password_login_e(){
+            $('#password_login_error').text('');
+        }
+        function r_username_login_e(){
+            $('#username_login_error').text('');
+        }
+
     </script>
 </body>
 </html>
