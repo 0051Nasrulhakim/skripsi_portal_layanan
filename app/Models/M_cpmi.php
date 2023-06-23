fefc1250b561d14856dfa927bc84ad80647be194
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-23 22:52:01 +0700

    Update M_cpmi.php

--- app/Models/M_cpmi.php
+++ app/Models/M_cpmi.php
@@ -9,5 +9,5 @@ class M_cpmi extends Model
     protected $table      = 'cpmi';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['nik','nama','alamat','nama_perusahaan','jabatan','negara_tujuan','tanggal_berangkat','pas_foto','foto_ktp','foto_kk','foto_akta_Kelahiran','foto_surat_nikah','foto_ijazah_terakhir','foto_surat_perjanjian','foto_medical_check_up','foto_ak1','tanggal_pengajuan'];
+    protected $allowedFields = ['id_user','nik','nama','alamat','nama_perusahaan','jabatan','negara_tujuan','tanggal_berangkat','pas_foto','foto_ktp','foto_kk','foto_akta_Kelahiran','foto_surat_nikah','foto_ijazah_terakhir','foto_surat_perjanjian','foto_medical_check_up','foto_ak1','tanggal_pengajuan'];
 }
