4a0ea7f7352b2fd9338bfc65538989f693a33f14
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-25 21:02:20 +0700

    Update M_ak.php

--- app/Models/M_ak.php
+++ app/Models/M_ak.php
@@ -9,5 +9,5 @@ class M_ak extends Model
     protected $table      = 'tb_ak1';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['id_user','nik', 'nama', 'tanggal_lahir', 'jenis_kelamin', 'status', 'alamat', 'agama', 'foto_ijazah', 'pass_foto', 'sertifikat_keahlian', 'tanggal_pengajuan'];
+    protected $allowedFields = ['id_user','nik', 'nama', 'tanggal_lahir', 'jenis_kelamin', 'status', 'alamat', 'agama', 'foto_ijazah', 'pass_foto', 'sertifikat_keahlian', 'tanggal_pengajuan', 'status_pengajuan', 'pesan'];
 }
