b1ad0a6f5a9d7bd8166f5ed5c1637d812af6641c
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-25 21:02:56 +0700

    Create modals_tolak.php

---
+++ app/Views/admin/modals/modals_tolak.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="modal_tolak" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="f_tolak">
+                <div class="modal-body">
+                
+                    <div class="mb-3 row">
+                        <label for="nama_lengkap" class="col-sm-3 col-form-label">Alasan Penolakan</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="pesan" name="pesan">
+                            <input type="text" class="form-control" id="id" name="id" hidden>
+                        </div>
+                    </div>
+                
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
