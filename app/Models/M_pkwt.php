f021e598f388a671bbcf87348815d951d68a86ce
0051Nasrulhakim <ti192400051@gmail.com>
2023-06-25 21:02:49 +0700

    Update M_pkwt.php

--- app/Models/M_pkwt.php
+++ app/Models/M_pkwt.php
@@ -9,5 +9,5 @@ class M_pkwt extends Model
     protected $table      = 'pkwt';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['id_user','nama_perusahaan_pkwt','direktur_pkwt','jumlah_pekerja_pkwt','daftar_pekerja_pkwt','naskah_pkwt','tanggal_pengajuan'];
+    protected $allowedFields = ['id_user','nama_perusahaan_pkwt','direktur_pkwt','jumlah_pekerja_pkwt','daftar_pekerja_pkwt','naskah_pkwt','tanggal_pengajuan','status_pengajuan','pesan'];
 }
